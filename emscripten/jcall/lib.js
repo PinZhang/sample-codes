ec12ec1c789b6feafac3d28f3f958437ac68263a
Pin Zhang <pzhang@mozilla.com>
2015-03-04 19:21:46 +0800

    Access JS string from C.

--- emscripten/jcall/lib.js
+++ emscripten/jcall/lib.js
@@ -1,6 +1,17 @@
 mergeInto(LibraryManager.library, {
   js_strlen: function(str) {
     return Pointer_stringify(str).length;
+  },
+
+  js_completion: function(str) {
+    if ('comple' == Pointer_stringify(str)) {
+      // Allocate space for string and extra '0' at the end
+      var completion = 'completion';
+      var buffer = Module._malloc(completion.length+1);
+      Module.writeStringToMemory(completion, buffer);
+      return buffer;
+    }
+    return '';
   }
 });
 
--- emscripten/jcall/strlen.c
+++ emscripten/jcall/strlen.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <emscripten.h>
 
 #ifdef __cplusplus
@@ -7,14 +8,22 @@ extern "C" {
 
 extern int js_strlen(const char* str);
 
+extern char* js_completion(const char* str);
+
 int main() {
   const char* str = "string for testing";
   int len = js_strlen(str);
-  printf("The length of %s is %d.\n", str, len);
+  printf("The length of '%s' is %d.\n", str, len);
 
   EM_ASM(
     console.log('Print message from C.');
   );
+
+  // Access string generated in JS.
+  const char* tmp = "comple";
+  char* completion = js_completion(tmp);
+  printf("The completion of '%s' is %s.\n", tmp, completion);
+  free(completion);
 }
 
 #ifdef __cplusplus
